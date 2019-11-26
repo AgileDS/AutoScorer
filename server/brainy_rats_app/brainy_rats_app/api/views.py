0536f5b3033ebc5bb82adc59a893fe403011611a
Alex Arcas Cuerda <aarcascuerda@gmail.com>
2019-11-26 20:13:26 +0100

    Password validation

--- server/brainy_rats_app/brainy_rats_app/api/views.py
+++ server/brainy_rats_app/brainy_rats_app/api/views.py
@@ -1,8 +1,13 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import GenericAPIView, CreateAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.settings import api_settings
+
 from django.contrib.auth import password_validation
+from django.core.exceptions import ValidationError
+from django.http import JsonResponse
 
 from brainy_rats_app.api.serializers import DatasetSerializer, UserSerializer
 
@@ -20,9 +25,28 @@ class DatasetViewSet(CreateAPIView):
     serializer_class = DatasetSerializer
 
 
-class CreateUserAPIView(CreateAPIView):
+class CreateUserAPIView(GenericAPIView):
     serializer_class = UserSerializer
 
     def post(self, request, *args, **kwargs):
-        response = super().post(request, *args, **kwargs)
-        return response
+        return self.create(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        try:
+            password_validation.validate_password(request.data['password'], request.data['username'])
+        except ValidationError:
+            return JsonResponse({'Error': 'Password not valid'})
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def get_success_headers(self, data):
+        try:
+            return {'Location': str(data[api_settings.URL_FIELD_NAME])}
+        except (TypeError, KeyError):
+            return {}
