4b98ff0e2449ea648dc0ab1b5a857f3fd92b1c71
Alex Arcas Cuerda <aarcascuerda@gmail.com>
2019-11-27 21:00:08 +0100

    new statisc

---
+++ server/brainy_rats_app/brainy_rats_app/static/precache-manifest.ec54fa4bdce01556b5e9e8f35121acea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "316db141b8b686125e75a7fd8ec0724f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19dda33d6cc7afeb5a09",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "5f64f7448d8417fbaa2e",
+    "url": "/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "19dda33d6cc7afeb5a09",
+    "url": "/static/js/2.bcc41faf.chunk.js"
+  },
+  {
+    "revision": "5f64f7448d8417fbaa2e",
+    "url": "/static/js/main.af73544d.chunk.js"
+  },
+  {
+    "revision": "a42d9a787315098491fc",
+    "url": "/static/js/runtime-main.581070fd.js"
+  }
+]);
--- server/brainy_rats_app/brainy_rats_app/static/service-worker.js
+++ server/brainy_rats_app/brainy_rats_app/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.381a5b41462bb87238f9b4332f21d925.js"
+  "/precache-manifest.ec54fa4bdce01556b5e9e8f35121acea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
