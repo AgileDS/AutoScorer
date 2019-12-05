d71dcbe4d636a62ad354936a93f74ee918b96681
Alex Arcas Cuerda <aarcascuerda@gmail.com>
2019-12-05 18:28:29 +0100

    API update create datasets

--- server/brainy_rats_app/brainy_rats_app/api/models.py
+++ server/brainy_rats_app/brainy_rats_app/api/models.py
@@ -5,20 +5,23 @@ from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 from brainy_rats_app.users.models import User
 
+
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
 
+
 class Dataset(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
+    size = models.BigIntegerField()
+
     def __str__(self):
         return self.name
 
 
 class DatasetRow(models.Model):
-    ds = models.ForeignKey(Dataset, on_delete=models.CASCADE)   
+    ds = models.ForeignKey(Dataset, related_name='rows', on_delete=models.CASCADE)   
     score = models.CharField(max_length=5)
-    eeg = models.FloatField()
-    emg = models.FloatField()
+    index = models.IntegerField()
